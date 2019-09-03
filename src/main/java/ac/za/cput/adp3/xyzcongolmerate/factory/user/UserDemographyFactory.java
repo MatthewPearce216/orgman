7da244eaa8e5e469052aa9fa37f09e6245ca5e9a
MatthewPearce216 <pearcematthew99@gmail.com>
2019-09-03 20:48:43 +0200

    User Demography Factory

--- src/main/java/ac/za/cput/adp3/xyzcongolmerate/factory/user/UserDemographyFactory.java
+++ src/main/java/ac/za/cput/adp3/xyzcongolmerate/factory/user/UserDemographyFactory.java
@@ -8,13 +8,10 @@ public class UserDemographyFactory {
 
     //TODO: implement method body ONLY!
     public static UserDemography buildUserDemography(String userEmail, String userTitle, String genderId, String raceId, Date dateOfBirth) {
-        throw new UnsupportedOperationException("Not supported yet.");
-        /**
-         * Your implementation goes here
-         * INSTRUCTIONS
-         * 1. Remove line [//TODO: implement method body ONLY!]
-         * 2. Remove line [throw new UnsupportedOperationException("Not supported yet.");]
-         * 3. Create and return an object of UserDemography
-         */
+
+        new UserDemography(userEmail, userTitle, genderId, raceId, dateOfBirth);
+        buildUserDemography(userEmail, userTitle, genderId, raceId, dateOfBirth);
+
+                return new UserDemography(userEmail, userTitle, genderId, raceId, dateOfBirth);
     }
 }
