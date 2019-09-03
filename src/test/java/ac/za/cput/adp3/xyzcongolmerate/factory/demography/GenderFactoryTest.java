5d8066c04e38134b01abba2a37fd02cb6e7d064b
MatthewPearce216 <pearcematthew99@gmail.com>
2019-09-03 23:11:21 +0200

    Test 1

--- src/test/java/ac/za/cput/adp3/xyzcongolmerate/factory/demography/GenderFactoryTest.java
+++ src/test/java/ac/za/cput/adp3/xyzcongolmerate/factory/demography/GenderFactoryTest.java
@@ -1,21 +1,28 @@
 package ac.za.cput.adp3.xyzcongolmerate.factory.demography;
 
+import ac.za.cput.adp3.xyzcongolmerate.domain.demography.Gender;
+import org.junit.Before;
 import org.junit.Test;
+
+import static junit.framework.TestCase.assertNotNull;
 
 public class GenderFactoryTest {
 
     //TODO: implement method body ONLY!
+
+   public Gender gender;
+
+   @Before
+   public void setUp() throws Exception{
+
+       gender = GenderFactory.buildGender("Male", "96083050420888");
+   }
+
+
     @Test
     public void buildGender() {
-        throw new UnsupportedOperationException("Not supported yet!");
-        /**
-         * Your implementation goes here
-         *
-         * INSTRUCTION
-         * 1. Remove line [//TODO: implement method body ONLY!]
-         * 2. Remove line [throw new UnsupportedOperationException("Not yet supported!");]
-         * 3. Test the GenderFactory class
-         * 4. Assert that the id is generated.
-         */
+
+    assertNotNull(gender);
+
     }
 }
